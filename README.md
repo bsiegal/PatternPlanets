710715884aed96812bed3428d49605c3c87139f0
Bess Siegal <bess.mah@gmail.com>
2012-03-08 14:39:51 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Pattern Planets
+==================================================
+
+This is a pattern game for kids created with [Paper.js](http://paperjs.org/).  It can be played using any HTML5 browser, including on an iPad, but the interaction is different on the iPad.  This project has simple examples of using group, color gradient, handling mouse, keyboard and canvas-resize events, cloning, changing position, animating movement, and integrating JavaScript code outside of PaperScript with PaperScript.  Also, it has an example of using HTML5 audio tag for sound effects, , which are suppressed on the iPad because they are too slow there.
+
+How to play
+--------------------------------------
+* Continue the pattern by dragging a planet from the planet palette to the target marked by the question mark.
+* If you miss the target, the planet will get lost in space!
+* If the planet you drop onto the target does not match the pattern, it will bounce back to the palette.
+* You may also select a planet by typing the initial of its color or by its numerical position in the palette, for example 'r' or '1' for the red planet.
+* On the iPad, you select a planet by tapping on it.  Dragging was a little too slow so the interaction was changed here.
+* When you are done, all the planets will orbit around a star.  Of course, this would make for a terrible solar system since they are all on the same orbit.
+* You may create your own pattern by changing the option in the select box.
+
+**Play at [bsiegal.github.com/ShapeEquate](http://bsiegal.github.com/PatternPlanets).**
+
---
+++ js/PatternPlanets.js
@@ -0,0 +1,116 @@
+/******************************************************************************* 
+ * 
+ * Copyright 2012 Bess Siegal
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
+var PatternPlanets = {
+    level: 2, /* lowest level */
+    mode: 'random',
+    
+    init: function() {
+        PatternPlanets.setLevel();
+        PatternPlanets.setMode();
+
+        if ((navigator.userAgent.match(/iPhone/i)) || 
+                (navigator.userAgent.match(/iPad/i))) {
+            PatternPlanets.mobile = true;
+        }
+        if (PatternPlanets.mobile) {
+            $('.mobile').css({'display': 'block'});
+            $('.browser').css({'display': 'none'});
+        }
+        
+        $('html').click(function() {
+            if ($('#about').is(':visible')) {
+                $('#about').slideUp();
+            }
+        });
+    },
+    
+    draw: function() {
+        var view = PatternPlanets.scope.view;
+        view.draw();
+    },
+        
+    setLevel: function() {
+        PatternPlanets.level = parseInt($('#level').val());
+        PatternPlanets.playAgain();
+    },
+    
+    setMode: function() {
+        PatternPlanets.mode = $('#mode').val();
+        PatternPlanets.playAgain();
+    },
+    
+    showHooray: function(show, x, y) {
+        var div = $('#buttonBar'); 
+        if (show) {
+            div.fadeIn();
+            var canvasPos = $('#PatternPlanetsCanvas').offset();
+            var top = canvasPos.top + y - div.height() / 2;
+            var left = canvasPos.left + (x - div.width() / 2) + 10;
+            div.css('top', top + 'px');
+            div.css('left', left + 'px');
+            $('#again').focus();
+        } else {
+            div.fadeOut();
+        }
+    },
+    
+    playSound: function(file) {
+        if (!PatternPlanets.mobile) {
+            var soundHandle = document.getElementById('soundHandle');
+            if (soundHandle && soundHandle.play) {
+                soundHandle.src = 'sounds/' + file;
+                soundHandle.play();                   
+                console.log('played ' + file);
+            }            
+        }
+    },
+    
+    playHooray: function() {
+        PatternPlanets.playSound('yaahooo.wav');
+    },
+ 
+    playValid: function() {
+        PatternPlanets.playSound('pluck.wav');
+    },
+    
+    playMiss: function() {
+        PatternPlanets.playSound('thunk.wav');
+    },
+    
+    playInvalid: function() {
+        PatternPlanets.playSound('boing2.wav');
+    },
+    
+    about: function(show) {
+        if (show) {
+            $('#about').slideDown();
+        } else {
+            $('#about').slideUp();
+        }
+    },
+    
+    toggleAbout: function() {
+        if ($('#about').is(':visible')) {
+            PatternPlanets.about(false);
+        } else {
+            PatternPlanets.about(true);
+        }
+    }
+};
+ 
